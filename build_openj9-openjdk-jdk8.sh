f695e323cb13c1b6ef66148c6a9df0d6b5d49d4d
Amarpreet Singh <amarpreet1997@gmail.com>
2021-12-07 17:39:26 -0500

    Rename the build directory to end with $(date '+%b%d')
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

--- build_openj9-openjdk-jdk8.sh
+++ build_openj9-openjdk-jdk8.sh
@@ -3,12 +3,14 @@
 DIRECTORY=openj9-openjdk-jdk8
 if [ -d "$DIRECTORY" ]; then
    echo "$DIRECTORY exists"
-   #mv openj9-openjdk-jdk8 openj9-openjdk-jdk8_dec3 
+   #date()
+   date=$(echo "$(date '+%b%d')" | awk '{print tolower($1)}')
+   mv $DIRECTORY "$DIRECTORY"_$date
    exit
 fi
 
 git clone https://github.com/ibmruntimes/openj9-openjdk-jdk8.git 
-cd openj9-openjdk-jdk8 
+cd $DIRECTORY
 bash get_source.sh 
 bash configure
 make all
