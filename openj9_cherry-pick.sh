9c6c6a822986f8f9ab49c531188aec00374b1c6d
Amarpreet Singh <amarpreet1997@gmail.com>
2021-12-07 08:59:39 -0500

    Check the number of input arguments with "$#"
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

--- openj9_cherry-pick.sh
+++ openj9_cherry-pick.sh
@@ -1,12 +1,21 @@
 #!/bin/sh
 
-cd /home/amar/cache_the_result_of_objectAlignmentInBytes/openj9-openjdk-jdk8/openj9
+if [ "$#" -ne 1 ]
+then
+   echo "commit absent"
+   exit
+fi
+
+COMMIT=$1
+DIRECTORY=/home/amar/cache_the_result_of_objectAlignmentInBytes
+cd $DIRECTORY/openj9-openjdk-jdk8/openj9
 git remote add local https://github.com/singh264/openj9.git
 git fetch --prune local
-git cherry-pick $1
+git cherry-pick $COMMIT
 A=$(git reset HEAD^1)
 git diff
 B=$(git reset HEAD@{1})
+cd $DIRECTORY
 
 C=( "cpp" "hpp" )
 for var in $A
@@ -15,7 +24,10 @@ do
    if [[ " ${C[@]} " =~ " $file " ]]
    then
       file=$(echo "$var" | rev | cut -d'/' -f1 | rev)
-      vimdiff /home/amar/cache_the_result_of_objectAlignmentInBytes/$file /home/amar/cache_the_result_of_objectAlignmentInBytes/openj9-openjdk-jdk8/openj9/$var
+      rm $file $file.*
+      wget https://raw.githubusercontent.com/eclipse-openj9/openj9/master/$var
+      sleep 5
+      vimdiff $DIRECTORY/$file $DIRECTORY/openj9-openjdk-jdk8/openj9/$var
    fi
 done
 
