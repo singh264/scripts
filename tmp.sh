e75932b3c6b1f758f9e7417b3e8a19b3dc166221
Amarpreet Singh <amarpreet1997@gmail.com>
2021-12-10 18:30:47 -0500

    openj9_checkout_remote_branch.sh
    https://github.com/singh264/scripts/commit/d215c2906d7a63f7425018419c36768e263a2f26
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

--- tmp.sh
+++ tmp.sh
@@ -1,121 +1,19 @@
 #!/bin/bash
 
-echo "openj9_checkout_new_branch.sh"
-
-#DIRECTORY="/Users/amar/git/openj9"
-#cd $DIRECTORY
-#A=$(git status)
-#for var in $A
-#do
-#   echo $var
-#   if [ $var == "branch" ]
-#   then
-#      echo "branch"
-#   fi
-#done
-#B=0
-#for var in $A
-#do
-#   if [ $var == "branch" ]
-#   then
-#      B=1
-#   fi
-#
-#   if [ $B == 1 ]
-#   then
-#      echo "$var"
-#   fi
-#   
-#   echo $B
-#   
-#   echo $var
-#
-#   echo "$var"
-#done
-#B=""
-#for var in $A
-#do
-#   if [ "$B" = "branch" ]
-#   then
-#      echo $var
-#   fi
-#   
-#   if [ $var == "branch" ]
-#   then
-#      B="branch"
-#   fi
-#done
-#B=""
-#for var in $A
-#do
-#   if [ "$B" = "branch" ]
-#   then
-#      echo $var
-#      B=""
-#   fi
-#   
-#   if [ $var == "branch" ]
-#   then
-#      B="branch"
-#   fi
-#done
-#B=""
-#for var in $A
-#do
-#   if [ "$B" = "branch" ]
-#   then
-#      B=$var
-#      echo $B
-#   fi
-#
-#   if [ $var == "branch" ]
-#   then
-#      B="branch"
-#   fi
-#done
-#echo $B
-#
-#VAR="08"
-#let "VAR=10#${VAR}"
-#echo $VAR
-#
-##git fetch --prune upstream; git rebase -i upstream/master; git log
-
-#echo "$#"
-#if [ "$#" -ne 1 ]
-#then
-#   echo "branch absent"
-#   exit
-#fi
-#
-#BRANCH=$1
-#DIRECTORY="/Users/amar/git/openj9"
-#cd $DIRECTORY
-##date()
-#date=$(echo "$(date '+%b%d')" | awk '{print tolower($1)}')
-## remote branch
-#git checkout -b "$BRANCH"_$date
-#git fetch --prune upstream
-#git rebase -i upstream/master
-#git log
-
-#echo "$1"
-#echo "$PWD"
+echo "openj9_checkout_remote_branch.sh"
 
 if [ "$#" -ne 1 ]
 then
-   echo "branch absent"
+   echo "remote branch absent"
    exit
 fi
 
 BRANCH=$1
 DIRECTORY="openj9-openjdk-jdk8/openj9"
 cd $PWD/$DIRECTORY
-#date()
-date=$(echo "$(date '+%b%d')" | awk '{print tolower($1)}')
-git checkout -b "$BRANCH"_$date
-git fetch --prune upstream
-git rebase -i upstream/master
+git remote add local git@github.com:singh264/openj9.git
+git fetch --prune local
+git checkout -b $BRANCH local/$BRANCH
 
 git log
 
