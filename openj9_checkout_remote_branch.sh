d215c2906d7a63f7425018419c36768e263a2f26
Amarpreet Singh <amarpreet1997@gmail.com>
2021-12-13 08:02:29 -0500

    Scripts
    I think openj9_checkout_remote_branch.sh creates a
    new remote branch.
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

---
+++ openj9_checkout_remote_branch.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+echo "openj9_checkout_remote_branch.sh"
+
+if [ "$#" -ne 1 ]
+then
+   echo "remote branch absent"
+   exit
+fi
+
+BRANCH=$1
+DIRECTORY="openj9-openjdk-jdk8/openj9"
+cd $PWD/$DIRECTORY
+git remote add local git@github.com:singh264/openj9.git
+git fetch --prune local
+git checkout -b $BRANCH local/$BRANCH
+
+git log
+
+cd -
