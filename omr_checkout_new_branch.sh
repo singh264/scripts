d06c53fbf34ed86ddc4612bb9e5d6efbe5acd36b
Amarpreet Singh <amarpreet1997@gmail.com>
2021-12-13 19:42:57 -0500

    Scripts
    I think omr_checkout_new_branch.sh creates a new
    branch and appends $(date '+%b%d') to the name of the
    branch.
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

--- omr_checkout_new_branch.sh
+++ omr_checkout_new_branch.sh
@@ -11,8 +11,10 @@ fi
 BRANCH=$1
 DIRECTORY="openj9-openjdk-jdk8/omr"
 cd $PWD/$DIRECTORY
+#date()
 date=$(echo "$(date '+%b%d')" | awk '{print tolower($1)}')
 git checkout -b "$BRANCH"_$date
+#git remote add $NAME $URL
 git fetch --prune upstream
 git rebase -i upstream/master
 
