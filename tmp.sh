f3d480de6c28d63f10751d215652e6aa672f94d2
Amarpreet Singh <amarpreet1997@gmail.com>
2021-12-13 19:59:50 -0500

    omr_checkout_remote_branch.sh
    https://github.com/singh264/scripts/commit/d3f6ca5230a64bc474e619bfa1e0d8bb54fe9620
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

--- tmp.sh
+++ tmp.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-echo "omr_checkout_new_branch.sh"
+echo "omr_checkout_remote_branch.sh"
 
 if [ "$#" -ne 1 ]
 then
-   echo "branch absent"
+   echo "remote branch absent"
    exit
 fi
 
 BRANCH=$1
 DIRECTORY="openj9-openjdk-jdk8/omr"
 cd $PWD/$DIRECTORY
-date=$(echo "$(date '+%b%d')" | awk '{print tolower($1)}')
-git checkout -b "$BRANCH"_$date
-git fetch --prune upstream
-git rebase -i upstream/master
+git remote add local git@github.com:singh264/omr.git
+git fetch --prune local
+git checkout -b $BRANCH local/$BRANCH
 
 git log
 
