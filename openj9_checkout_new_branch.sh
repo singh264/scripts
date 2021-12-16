471cf69c8dde01e998590f538e035e4fd7666321
Amarpreet Singh <amarpreet1997@gmail.com>
2021-12-16 09:47:34 -0500

    Create the new branch in a method
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

--- openj9_checkout_new_branch.sh
+++ openj9_checkout_new_branch.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+create_the_new_branch()
+{
+   date=$(echo "$(date '+%b%d')" | awk '{print tolower($1)}')
+   git checkout -b "$BRANCH"_$date
+}
+
 if [ "$#" -ne 1 ]
 then
    echo "branch absent"
@@ -9,9 +15,7 @@ fi
 BRANCH=$1
 DIRECTORY="openj9-openjdk-jdk8/openj9"
 cd $PWD/$DIRECTORY
-#date()
-date=$(echo "$(date '+%b%d')" | awk '{print tolower($1)}')
-git checkout -b "$BRANCH"_$date
+create_the_new_branch
 #git remote add $NAME $URL
 git fetch --prune upstream
 git rebase -i upstream/master
