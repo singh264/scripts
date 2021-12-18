3e3a421629601fd8fd452965debe9b83dabf9010
Amarpreet Singh <amarpreet1997@gmail.com>
2021-12-18 16:22:24 -0500

    Scripts
    I think create_a_new_encoded_file.sh creates a new
    file with a filename that is base64 encoded.
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

---
+++ create_a_new_encoded_file.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]
+then
+    echo "string absent"
+    exit
+fi
+
+STRING=$1
+encoded_string=$(echo "$STRING" | base64)
+decoded_string=$(echo "$encoded_string" | base64 --decode)
+vim "${encoded_string:0:55}".txt
+echo "${encoded_string:0:55}"
+echo $decoded_string
