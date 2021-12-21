7fa8b794e966b0cbec339938cec23e053d370aa5
Amarpreet Singh <amarpreet1997@gmail.com>
2021-12-21 12:34:53 -0500

    Increase the indentation of the lines in the code
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

--- build_openj9-openjdk-jdk8.sh
+++ build_openj9-openjdk-jdk8.sh
@@ -2,16 +2,16 @@
 
 rename_the_build_directory()
 {
-   DIRECTORY=$1
-   date=$(echo "$(date '+%b%d')" | awk '{print tolower($1)}')
-   mv $DIRECTORY "$DIRECTORY"_$date
+    DIRECTORY=$1
+    date=$(echo "$(date '+%b%d')" | awk '{print tolower($1)}')
+    mv $DIRECTORY "$DIRECTORY"_$date
 }
 
 DIRECTORY=openj9-openjdk-jdk8
 if [ -d "$DIRECTORY" ]; then
-   echo "$DIRECTORY exists"
-   rename_the_build_directory $DIRECTORY
-   exit
+    echo "$DIRECTORY exists"
+    rename_the_build_directory $DIRECTORY
+    exit
 fi
 
 git clone https://github.com/ibmruntimes/openj9-openjdk-jdk8.git 
