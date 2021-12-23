7e4d65b5864ad96310b7ac3a15d4ac31890dc1f9
Amarpreet Singh <amarpreet1997@gmail.com>
2021-12-23 18:31:44 -0500

    Create the upstream remote branch in a new script
    I think openj9_checkout_upstream_branch.sh creates a
    new remote branch from the upstream repository
    https://github.com/singh264/scripts/commit/61787093ef658beab47b861289115e73b2a6d18b.
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

--- openj9_checkout_remote_branch.sh
+++ openj9_checkout_remote_branch.sh
@@ -11,7 +11,6 @@ fi
 BRANCH=$1
 DIRECTORY="openj9-openjdk-jdk8/openj9"
 cd $PWD/$DIRECTORY
-#git remote add $NAME $URL
 git remote add local git@github.com:singh264/openj9.git
 git fetch --prune local
 git checkout -b $BRANCH local/$BRANCH
