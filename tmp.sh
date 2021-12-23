b318c8787a749ee25343e63f1c236af7dfd44f79
Amarpreet Singh <amarpreet1997@gmail.com>
2021-12-23 18:04:54 -0500

    openj9_checkout_upstream_branch.sh
    https://github.com/singh264/scripts/commit/5b67ab103c675f0db586ececdebc1fb0502a74e9
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

--- tmp.sh
+++ tmp.sh
@@ -1,40 +1,21 @@
 #!/bin/bash
 
-echo "openj9_cherry-pick.sh"
+echo "openj9_checkout_upstream_branch.sh"
 
 if [ "$#" -ne 1 ]
 then
-    echo "commit absent"
+    echo "upstream branch absent"
     exit
 fi
 
-#brew install wget
-
-COMMIT=$1
-DIRECTORY=/home/amar/cache_the_result_of_objectAlignmentInBytes
-#DIRECTORY=$PWD
-cd $DIRECTORY/openj9-openjdk-jdk8/openj9
-git remote add local https://github.com/singh264/openj9.git
-git fetch --prune local
-git cherry-pick $COMMIT
-A=$(git reset HEAD^1)
-git diff
-B=$(git reset HEAD@{1})
-cd $DIRECTORY
-
-C=( "cpp" "hpp" )
-for var in $A
-do
-    file=$(echo "$var" | rev | cut -d'.' -f1 | rev)
-    if [[ " ${C[@]} " =~ " $file " ]]
-    then
-	file=$(echo "$var" | rev | cut -d'/' -f1 | rev)
-	rm $file $file.*
-	wget https://raw.githubusercontent.com/eclipse-openj9/openj9/master/$var
-	sleep 5
-	vimdiff $DIRECTORY/$file $DIRECTORY/openj9-openjdk-jdk8/openj9/$var
-	#vimdiff $file $var
-    fi
-done
+BRANCH=$1
+DIRECTORY="openj9-openjdk-jdk8/openj9"
+cd $PWD/$DIRECTORY
+git remote add upstream git@github.com:eclipse-openj9/openj9.git
+git fetch --prune upstream
+git checkout -b $BRANCH upstream/$BRANCH
+git reset --hard upstream/$BRANCH
 
 git log
+
+cd -
