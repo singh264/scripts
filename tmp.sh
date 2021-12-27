fb7b91e2f292f2e0b602e935d9c786ded661315b
Amarpreet Singh <amarpreet1997@gmail.com>
2021-12-27 10:55:17 -0500

    omr_checkout_upstream_branch.sh
    https://github.com/singh264/scripts/commit/33f40069e0cb2d5a1ca4808c713fa183e5f10c24
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

--- tmp.sh
+++ tmp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "openj9_checkout_upstream_branch.sh"
+echo "omr_checkout_upstream_branch.sh"
 
 if [ "$#" -ne 1 ]
 then
@@ -9,9 +9,9 @@ then
 fi
 
 BRANCH=$1
-DIRECTORY="openj9-openjdk-jdk8/openj9"
+DIRECTORY="openj9-openjdk-jdk8/omr"
 cd $PWD/$DIRECTORY
-git remote add upstream git@github.com:eclipse-openj9/openj9.git
+git remote add upstream git@github.com:eclipse/omr.git
 git fetch --prune upstream
 git checkout -b $BRANCH upstream/$BRANCH
 git reset --hard upstream/$BRANCH
