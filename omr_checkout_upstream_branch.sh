0d7219de3abc81033768b22b9e1aae520fa53b0f
Amarpreet Singh <amarpreet1997@gmail.com>
2021-12-27 10:59:09 -0500

    Scripts
    I think omr_checkout_upstream_branch.sh creates a new
    remote branch from the upstream repository.
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

---
+++ omr_checkout_upstream_branch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]
+then
+    echo "upstream branch absent"
+    exit
+fi
+
+BRANCH=$1
+DIRECTORY="openj9-openjdk-jdk8/omr"
+cd $PWD/$DIRECTORY
+git remote add upstream git@github.com:eclipse/omr.git
+git fetch --prune upstream
+git checkout -b $BRANCH upstream/$BRANCH
+git reset --hard upstream/$BRANCH
+
+git log
+
+cd -
