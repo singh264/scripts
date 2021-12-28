6c10f314c6bdb613e91da9c3ef5a37cb9eb1962f
Amarpreet Singh <amarpreet1997@gmail.com>
2021-12-28 12:55:22 -0600

    Add the remote named upstream to update the new branch
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

--- omr_checkout_new_branch.sh
+++ omr_checkout_new_branch.sh
@@ -16,7 +16,7 @@ BRANCH=$1
 DIRECTORY="openj9-openjdk-jdk8/omr"
 cd $PWD/$DIRECTORY
 create_the_new_branch $BRANCH
-#git remote add $NAME $URL
+git remote add upstream git@github.com:eclipse/omr.git
 git fetch --prune upstream
 git rebase -i upstream/master
 
