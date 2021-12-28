51594d189c177270e7f53c1aafee89bb29e4e919
Amarpreet Singh <amarpreet1997@gmail.com>
2021-12-28 13:41:35 -0600

    Create the upstream remote branch in a new script
    I think omr_checkout_upstream_branch.sh creates a new
    remote branch from the upstream repository
    https://github.com/singh264/scripts/commit/0d7219de3abc81033768b22b9e1aae520fa53b0f.
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

--- omr_checkout_remote_branch.sh
+++ omr_checkout_remote_branch.sh
@@ -11,7 +11,6 @@ fi
 BRANCH=$1
 DIRECTORY="openj9-openjdk-jdk8/omr"
 cd $PWD/$DIRECTORY
-#git remote add $NAME $URL
 git remote add local git@github.com:singh264/omr.git
 git fetch --prune local
 git checkout -b $BRANCH local/$BRANCH
