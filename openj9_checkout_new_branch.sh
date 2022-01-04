967b48c39914d549c4b5014ae4912b992f12bc00
Amarpreet Singh <amarpreet1997@gmail.com>
2022-01-04 13:34:12 -0600

    Provide the name of the branch to the method
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

--- openj9_checkout_new_branch.sh
+++ openj9_checkout_new_branch.sh
@@ -3,7 +3,7 @@
 create_the_new_branch()
 {
     date=$(echo "$(date '+%b%d')" | awk '{print tolower($1)}')
-    git checkout -b "$BRANCH"_$date
+    git checkout -b $1_$date
 }
 
 if [ "$#" -ne 1 ]
@@ -15,7 +15,7 @@ fi
 BRANCH=$1
 DIRECTORY="openj9-openjdk-jdk8/openj9"
 cd $PWD/$DIRECTORY
-create_the_new_branch
+create_the_new_branch $BRANCH
 git remote add upstream git@github.com:eclipse-openj9/openj9.git
 git fetch --prune upstream
 git rebase -i upstream/master
