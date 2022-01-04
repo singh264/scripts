c2715e4d7b3b09b856d7f9646a3c30d83bfa559b
Amarpreet Singh <amarpreet1997@gmail.com>
2022-01-04 14:35:51 -0600

    openj9_checkout_new_branch.sh.
    https://github.com/singh264/scripts/commit/941ed77ec64534398c695b93e17a11d0b4e3f389.
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com.

--- tmp.sh
+++ tmp.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-echo "omr_checkout_new_branch.sh"
+echo "openj9_checkout_new_branch.sh"
 
 create_the_new_branch()
 {
     date=$(echo "$(date '+%b%d')" | awk '{print tolower($1)}')
-    #git checkout -b "$1"_$date
-    git checkout -b "$BRANCH"_$date
+    git checkout -b $1_$date
 }
 
 if [ "$#" -ne 1 ]
@@ -16,13 +15,16 @@ then
 fi
 
 BRANCH=$1
-DIRECTORY="openj9-openjdk-jdk8/omr"
+#DIRECTORY=$PWD
+DIRECTORY="openj9-openjdk-jdk8/openj9"
+#cd $DIRECTORY/openj9-openjdk-jdk8/openj9
 cd $PWD/$DIRECTORY
 create_the_new_branch $BRANCH
-git remote add upstream git@github.com:eclipse/omr.git
+git remote add upstream git@github.com:eclipse-openj9/openj9.git
 git fetch --prune upstream
 git rebase -i upstream/master
 
 git log
 
+#cd $DIRECTORY
 cd -
