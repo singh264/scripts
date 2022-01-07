a9caedd4e8a09145abdb592bde3ab2677b68bb6b
Amarpreet Singh <amarpreet1997@gmail.com>
2022-01-07 12:25:43 -0600

    openj9_cherry-pick.sh.
    https://github.com/singh264/scripts/commit/525cd3c2646d063eecee9a42d9b91f81894d6674.
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com.

--- tmp.sh
+++ tmp.sh
@@ -1,24 +1,61 @@
-#!/bin/bash
+#!/bin/sh
 
-echo "openj9_checkout_remote_branch.sh"
+echo "openj9_cherry-pick.sh"
+
+install_wget()
+{
+    OS=$(uname -s)
+    if [[ "$OS" == "Darwin" ]]
+    then
+	echo "Install wget on macOS."
+	brew install wget
+    elif [[ "$OS" == "Linux" ]]
+    then
+        echo "Install wget on Linux."
+        sudo apt-get install wget
+    fi
+}
 
 if [ "$#" -ne 1 ]
 then
-    echo "remote branch absent"
+    echo "commit absent"
     exit
 fi
 
-BRANCH=$1
-#DIRECTORY=$PWD
-DIRECTORY="openj9-openjdk-jdk8/openj9"
-#cd $DIRECTORY/openj9-openjdk-jdk8/openj9
-cd $PWD/$DIRECTORY
-git remote add local git@github.com:singh264/openj9.git
+install_wget
+
+COMMIT=$1
+DIRECTORY=$PWD
+cd $DIRECTORY/openj9-openjdk-jdk8/openj9
+git remote add local https://github.com/singh264/openj9.git
 git fetch --prune local
-git checkout -b $BRANCH local/$BRANCH
-git reset --hard local/$BRANCH
+git cherry-pick $COMMIT
+A=$(git reset HEAD^1)
+git diff
+B=$(git reset HEAD@{1})
+cd $DIRECTORY
+
+C=( "cpp" "hpp" )
+for var in $A
+do
+    file=$(echo "$var" | rev | cut -d'.' -f1 | rev)
+    if [[ " ${C[@]} " =~ " $file " ]]
+    then
+	file=$(echo "$var" | rev | cut -d'/' -f1 | rev)
+	rm $file $file.*
+	wget https://raw.githubusercontent.com/eclipse-openj9/openj9/master/$var
+	sleep 5
+	if [ -d "$DIRECTORY/openj9-openjdk-jdk8/openj9" ]
+	then
+	    vimdiff $DIRECTORY/$file $DIRECTORY/openj9-openjdk-jdk8/openj9/$var
+	else
+	    vimdiff $file $var
+	fi
+    fi
+done
+
+cd $DIRECTORY/openj9-openjdk-jdk8/openj9
 
 git log
 
 #cd $DIRECTORY
-cd -
