27900a7ff9c730c927567b5f40f4fe4edb30ab74
Amarpreet Singh <amarpreet1997@gmail.com>
2022-01-17 09:30:24 -0600

    Merge the branch to the default branch. (#27)
    * Scripts
    
    I think omr_checkout_remote_branch.sh creates a
    new remote branch.
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com
    
    * Reset the current branch to the remote branch
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com
    
    * Increase the indentation of the lines in the code
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com
    
    * Create the upstream remote branch in a new script
    
    I think omr_checkout_upstream_branch.sh creates a new
    remote branch from the upstream repository
    https://github.com/singh264/scripts/commit/0d7219de3abc81033768b22b9e1aae520fa53b0f.
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

--- omr_checkout_remote_branch.sh
+++ omr_checkout_remote_branch.sh
@@ -4,8 +4,8 @@ echo "omr_checkout_remote_branch.sh"
 
 if [ "$#" -ne 1 ]
 then
-   echo "remote branch absent"
-   exit
+    echo "remote branch absent"
+    exit
 fi
 
 BRANCH=$1
@@ -14,6 +14,7 @@ cd $PWD/$DIRECTORY
 git remote add local git@github.com:singh264/omr.git
 git fetch --prune local
 git checkout -b $BRANCH local/$BRANCH
+git reset --hard local/$BRANCH
 
 git log
 
