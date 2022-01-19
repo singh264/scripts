6fdeed8aa0584095a933eeee32847de64fce2fba
Amarpreet Singh <amarpreet1997@gmail.com>
2022-01-19 08:06:55 -0600

    Merge the branch to the default branch. (#29)
    * Scripts
    
    I think openj9_checkout_new_branch.sh creates a new
    branch and appends $(date '+%b%d') to the name of the
    branch.
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com
    
    * Create the new branch in a method
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com
    
    * Increase the indentation of the lines in the code
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com
    
    * Add the remote named upstream to update the new branch
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com
    
    * Provide the name of the branch to the method
    
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com

--- openj9_checkout_new_branch.sh
+++ openj9_checkout_new_branch.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 
+create_the_new_branch()
+{
+    date=$(echo "$(date '+%b%d')" | awk '{print tolower($1)}')
+    git checkout -b $1_$date
+}
+
 if [ "$#" -ne 1 ]
 then
-   echo "branch absent"
-   exit
+    echo "branch absent"
+    exit
 fi
 
 BRANCH=$1
 DIRECTORY="openj9-openjdk-jdk8/openj9"
 cd $PWD/$DIRECTORY
-date=$(echo "$(date '+%b%d')" | awk '{print tolower($1)}')
-git checkout -b "$BRANCH"_$date
+create_the_new_branch $BRANCH
+git remote add upstream git@github.com:eclipse-openj9/openj9.git
 git fetch --prune upstream
 git rebase -i upstream/master
 
