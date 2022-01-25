38bf52b8ba1c2dba2599c61a8575ed4c920dfa22
Amarpreet Singh <amarpreet1997@gmail.com>
2022-01-25 18:23:50 +0000

    Initialize the environment variables in the script.
    Closes: https://github.com/singh264/scripts/issues/38.
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com.

--- test_openj9-openjdk-jdk8.sh
+++ test_openj9-openjdk-jdk8.sh
@@ -2,8 +2,22 @@
 
 echo "test_openj9-openjdk-jdk8.sh"
 
-DIRECTORY=$PWD
-COMPILE_JDK_HOME=$DIRECTORY/openj9-openjdk-jdk8/build/linux-ppc64le-normal-server-release/images/j2sdk-image
+images_directory_path=""
+
+initialize_the_images_directory_path()
+{
+    DIRECTORY=$PWD
+    if [ -d "$DIRECTORY/openj9-openjdk-jdk8/build/linux-ppc64le-normal-server-release/images" ]
+    then
+	images_directory_path="$DIRECTORY/openj9-openjdk-jdk8/build/linux-ppc64le-normal-server-release/images"
+    elif [ -d "$DIRECTORY/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images" ]
+    then
+	images_directory_path="$DIRECTORY/openj9-openjdk-jdk8/build/linux-x86_64-normal-server-release/images"
+    fi
+}
+
+initialize_the_images_directory_path
+COMPILE_JDK_HOME=$images_directory_path/j2sdk-image
 if [ $# -eq 1 ]
 then
     COMPILE_JDK_HOME=$1
@@ -18,5 +32,6 @@ cd $DIRECTORY/aqa-tests/TKG
 make clean
 make compile
 
-export TEST_JDK_HOME=$DIRECTORY/openj9-openjdk-jdk8/build/linux-ppc64le-normal-server-release/images/j2sdk-image
+export TEST_JDK_HOME=$images_directory_path/j2sdk-image
+export NATIVE_TEST_LIBS=$images_directory_path/test/openj9
 make _sanity.functional
