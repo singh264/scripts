e3748a8cf6b783f098757d3cbc8189f3065966f9
Amarpreet Singh <amarpreet1997@gmail.com>
2022-01-25 14:33:14 +0000

    Provide the directory paths to the script.
    Closes: https://github.com/singh264/scripts/issues/34.
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com.

--- build_openj9-openjdk-jdk8.sh
+++ build_openj9-openjdk-jdk8.sh
@@ -1,14 +1,46 @@
 #!/bin/sh
 
+boot_jdk_directory_path=""
+freetype_include_directory_path=""
+freetype_lib_directory_path=""
+
+initialize_the_directory_paths()
+{
+    for var in "$@"
+    do
+        key=$(echo $var | cut -d'=' -f1)
+        value=$(echo $var | cut -d'=' -f2)
+
+        if [ "$key" == "--with-boot-jdk" ]
+        then
+	    boot_jdk_directory_path=$value
+        elif [ "$key" == "--with-freetype-include" ]
+        then
+	    freetype_include_directory_path=$value
+        elif [ "$key" == "--with-freetype-lib" ]
+        then
+	    freetype_lib_directory_path=$value
+        fi
+    done
+}
+
+rename_the_build_directory()
+{
+    DIRECTORY=$1
+    date=$(echo "$(date '+%b%d')" | awk '{print tolower($1)}')
+    mv $DIRECTORY "$DIRECTORY"_$date
+}
+
+INPUT="$@"
 DIRECTORY=openj9-openjdk-jdk8
 if [ -d "$DIRECTORY" ]; then
-   echo "$DIRECTORY exists"
-   #mv openj9-openjdk-jdk8 openj9-openjdk-jdk8_dec3 
-   exit
+    echo "$DIRECTORY exists"
+    rename_the_build_directory $DIRECTORY
 fi
 
 git clone https://github.com/ibmruntimes/openj9-openjdk-jdk8.git 
 cd openj9-openjdk-jdk8 
 bash get_source.sh 
-bash configure
+initialize_the_directory_paths $INPUT
+bash configure --with-boot-jdk=$boot_jdk_directory_path --with-freetype-include=$freetype_include_directory_path --with-freetype-lib=$freetype_lib_directory_path
 make all
