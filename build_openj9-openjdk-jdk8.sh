fbdb895f829dc17a2065ce5a8f833cec8edf93c4
Amarpreet Singh <amarpreet1997@gmail.com>
2022-01-25 14:33:14 +0000

    Provide the directory paths to the script.
    Closes: https://github.com/singh264/scripts/issues/34.
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com.

--- build_openj9-openjdk-jdk8.sh
+++ build_openj9-openjdk-jdk8.sh
@@ -1,5 +1,29 @@
 #!/bin/sh
 
+boot_jdk_directory_path=""
+freetype_include_directory_path=""
+freetype_lib_directory_path=""
+
+initialize_the_directory_paths()
+{
+    for var in "$@"
+    do
+        key=$(echo $var | cut -d'=' -f1)
+        value=$(echo $var | cut -d'=' -f2)
+
+        if [ "$key" == "--with-boot-jdk" ]
+        then
+	    boot_jdk_directory_path=$value
+        elif [ "$key" == "--with-freetype-include" ]
+        then
+	    freetype_include_directory_path=$value
+        elif [ "$key" == "--with-freetype-lib" ]
+        then
+	    freetype_lib_directory_path=$value
+        fi
+    done
+}
+
 rename_the_build_directory()
 {
     DIRECTORY=$1
@@ -7,6 +31,7 @@ rename_the_build_directory()
     mv $DIRECTORY "$DIRECTORY"_$date
 }
 
+INPUT="$@"
 DIRECTORY=openj9-openjdk-jdk8
 if [ -d "$DIRECTORY" ]; then
     echo "$DIRECTORY exists"
@@ -17,5 +42,6 @@ fi
 git clone https://github.com/ibmruntimes/openj9-openjdk-jdk8.git 
 cd $DIRECTORY
 bash get_source.sh 
-bash configure
+initialize_the_directory_paths $INPUT
+bash configure --with-boot-jdk=$boot_jdk_directory_path --with-freetype-include=$freetype_include_directory_path --with-freetype-lib=$freetype_lib_directory_path
 make all
