30bd03846400989bad5b8cab4b4f80b17bb7011d
Amarpreet Singh <amarpreet1997@gmail.com>
2022-01-28 09:47:36 -0600

    Initialize the directory to $PWD.
    Closes: https://github.com/singh264/scripts/issues/3.
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com.

--- omr_checkout_new_branch.sh
+++ omr_checkout_new_branch.sh
@@ -13,8 +13,8 @@ then
 fi
 
 BRANCH=$1
-DIRECTORY="openj9-openjdk-jdk8/omr"
-cd $PWD/$DIRECTORY
+DIRECTORY=$PWD
+cd $DIRECTORY/openj9-openjdk-jdk8/omr
 create_the_new_branch $BRANCH
 git remote add upstream git@github.com:eclipse/omr.git
 git fetch --prune upstream
@@ -22,4 +22,4 @@ git rebase -i upstream/master
 
 git log
 
-cd -
+cd $DIRECTORY
