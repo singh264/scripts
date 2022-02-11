d6e86664ee2ecb10724ba16bf7f950d9a65d2298
Amarpreet Singh <amarpreet1997@gmail.com>
2022-02-11 12:26:20 -0600

    Display the filename of the script.
    Closes: https://github.com/singh264/scripts/issues/51.
    Signed-off-by: Amarpreet Singh amarpreet1997@gmail.com.

--- omr_checkout_new_branch.sh
+++ omr_checkout_new_branch.sh
@@ -2,10 +2,16 @@
 
 echo "omr_checkout_new_branch.sh"
 
+create_the_new_branch()
+{
+    date=$(echo "$(date '+%b%d')" | awk '{print tolower($1)}')
+    git checkout -b "$BRANCH"_$date
+}
+
 if [ "$#" -ne 1 ]
 then
-   echo "branch absent"
-   exit
+    echo "branch absent"
+    exit
 fi
 
 BRANCH=$1
