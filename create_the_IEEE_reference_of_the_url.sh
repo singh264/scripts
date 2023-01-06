8e7101bb35ebda0a528a64faec0a71080bcc1721
Amarpreet Singh <amarpreet1997@gmail.com>
2023-01-06 12:32:51 -0500

    Create the IEEE reference of the url.
    Closes: https://github.com/singh264/scripts/issues/9.
    Author: Amarpreet Singh amarpreet1997@gmail.com.

--- create_the_IEEE_reference_of_the_url.sh
+++ create_the_IEEE_reference_of_the_url.sh
@@ -9,19 +9,19 @@ the_date_that_the_url_was_accessed=""
 initialize_the_attribute_of_the_url()
 {
     attribute=$(echo $@ | cut -d' ' -f2-)
-    if [ $1 = "--the_name_of_the_author" ]
+    if [ "$1" = "--the_name_of_the_author" ]
     then
         the_name_of_the_author=$attribute
-    elif [ $1 = "--the_title_of_the_page" ]
+    elif [ "$1" = "--the_title_of_the_page" ]
     then
         the_title_of_the_page=$attribute
-    elif [ $1 = "--the_title_of_the_website" ]
+    elif [ "$1" = "--the_title_of_the_website" ]
     then
         the_title_of_the_website=$attribute
-    elif [ $1 = "--the_url" ]
+    elif [ "$1" = "--the_url" ]
     then
         the_url=$attribute
-    elif [ $1 = "--the_date_that_the_url_was_accessed" ]
+    elif [ "$1" = "--the_date_that_the_url_was_accessed" ]
     then
         the_date_that_the_url_was_accessed=$attribute
     fi
@@ -30,6 +30,7 @@ initialize_the_attribute_of_the_url()
 obtain_the_attributes_of_the_url()
 {
     the_name_of_the_attribute=""
+    the_name_of_the_old_attribute=""
     attribute=""
     the_start_of_the_attribute=0
     the_end_of_the_attribute=0
@@ -56,41 +57,26 @@ obtain_the_attributes_of_the_url()
 	    the_name_of_the_attribute=$key
 	fi
 
-	echo $value
-	attribute="$attribute $value"
-
-	if [[ $value == *"\""* ]]
+	if [[ $the_name_of_the_attribute != $the_name_of_the_old_attribute ]]
 	then
-	    if [ $the_start_of_the_attribute -eq 0 ]
-	    then
-		the_start_of_the_attribute=1
-		the_end_of_the_attribute=0
-	    elif [ $the_start_of_the_attribute -eq 1 ]
-	    then
-		the_start_of_the_attribute=0
-		the_end_of_the_attribute=1
-	    fi 
-
-	    if [ $the_end_of_the_attribute -eq 1 ]
-	    then
-	        initialize_the_attribute_of_the_url $the_name_of_the_attribute $attribute
-	        attribute=""
-	    fi
-	elif [[ $the_name_of_the_attribute == "--the_url" ]]
-	then
-	    initialize_the_attribute_of_the_url $the_name_of_the_attribute $attribute
+	    initialize_the_attribute_of_the_url $the_name_of_the_old_attribute $attribute
+	    the_name_of_the_old_attribute=$the_name_of_the_attribute
 	    attribute=""
 	fi
+
+	attribute="$attribute $value"
     done
+
+    initialize_the_attribute_of_the_url $the_name_of_the_old_attribute $attribute
 }
 
 create_the_IEEE_reference_of_the_url()
 {
     echo "$the_name_of_the_author. \
-	  \"$(echo $the_title_of_the_page | sed "s/ *$//g").\" \
+	  \"$(echo $the_title_of_the_page | sed "s/ *$//g")\" \
 	  $the_title_of_the_website. \
 	  $the_url \
-	  ($(echo $the_date_that_the_url_was_accessed | sed "s/ *$//g"))"
+	  ($(echo $the_date_that_the_url_was_accessed | sed "s/ *$//g"))."
 }
 
 INPUT=$@
